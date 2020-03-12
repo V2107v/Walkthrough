aea6f04ed714d31c4b72f0dfb5ae4a2e6264cc7a
HP <vaibhavsrivastava2107@gmail.com>
2020-03-12 11:05:42 +0530

    Walkthrough app

---
+++ app/src/main/java/com/example/walkthrough/IntroActivity.java
@@ -0,0 +1,158 @@
+package com.example.walkthrough;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //make the activity full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //when this activity is about to be launched we need to check if intro has opened before or not
+        if (restorePrefData()) {
+            Intent mainActivity = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(mainActivity);
+            finish();
+        }
+
+        setContentView(R.layout.activity_intro);
+
+        //hide the action bar
+        getSupportActionBar().hide();
+
+        //initial views
+        screenPager = findViewById(R.id.screen_view_pager);
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnNext = findViewById(R.id.next);
+        btnGetStarted = findViewById(R.id.getStarted);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
+
+        //fill list screen
+        final List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("Slide1","Hello, This is Incampus.",R.drawable.ic_arrow_forward_black_24dp));
+        mList.add(new ScreenItem("Slide2","Hello, This is Incampus.",R.drawable.ic_arrow_forward_black_24dp));
+        mList.add(new ScreenItem("Slide3","Hello, This is Incampus.",R.drawable.ic_arrow_forward_black_24dp));
+
+
+        // setup viewpager
+        introViewPagerAdapter = new IntroViewPagerAdapter(this,mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+        
+        //setup tablelayout with viewpager
+        tabIndicator.setupWithViewPager(screenPager);
+
+        //next button click listener
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                position = screenPager.getCurrentItem();
+                if(position < mList.size()) {
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+
+                if (position == mList.size()-1){
+                    //when we reach the last screen
+                    // TODO : show the GETSTARTED button and hide the indicator and next button
+                    loadLastScreen();
+                }
+            }
+        });
+
+        //tablayout add change listener
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == mList.size()-1) {
+                    loadLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        //Get Started button Click listener
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open main activity
+                Intent mainActivity = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mainActivity);
+
+                //also we need to save a boolean value to storage so next time when the user runs the app
+                //we could know that he is already checked the intro screen by shared preferences
+                savePrefsData();
+                finish();
+
+            }
+        });
+
+
+    }
+
+    private boolean restorePrefData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        boolean isIntroActivityOpenedBefore = pref.getBoolean("isIntroOpened",false);
+        return  isIntroActivityOpenedBefore;
+    }
+
+    private void savePrefsData() {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpened",true);
+        editor.commit();
+    }
+
+    //show the GETSTARTED button and hide the indicator and next button
+    private void loadLastScreen() {
+
+        btnGetStarted.setVisibility(View.VISIBLE);
+        btnNext.setVisibility(View.INVISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+
+        //TODO : Add an animation for getStarted button.
+
+        //lsetup animation
+        btnGetStarted.setAnimation(btnAnim);
+    }
+}
---
+++ app/src/main/java/com/example/walkthrough/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.walkthrough;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
